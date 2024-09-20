f9ae318a9fcf84f1efb5a339dc7180cb0bb165a6
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-09-20 11:34:00 +0300

    feat: Project Monthly Expense report
    Duplicated the Profit and Loss Report and removed the income columns
    and the summary chart

---
+++ nl_banadir/banadir_customization_reports/report/project_monthly_expense/project_monthly_expense.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2024, Navari Ltd and contributors
+// For license information, please see license.txt
+
+
+frappe.query_reports["Project Monthly Expense"] = $.extend({}, erpnext.financial_statements);
+
+erpnext.utils.add_dimensions("Project Monthly Expense", 10);
+
+frappe.query_reports["Project Monthly Expense"]["filters"].push({
+	fieldname: "selected_view",
+	label: __("Select View"),
+	fieldtype: "Select",
+	options: [
+		{ value: "Report", label: __("Report View") },
+		{ value: "Growth", label: __("Growth View") },
+		{ value: "Margin", label: __("Margin View") },
+	],
+	default: "Report",
+	reqd: 1,
+});
+
+frappe.query_reports["Project Monthly Expense"]["filters"].push({
+	fieldname: "accumulated_values",
+	label: __("Accumulated Values"),
+	fieldtype: "Check",
+	default: 1,
+});
+
+frappe.query_reports["Project Monthly Expense"]["filters"].push({
+	fieldname: "include_default_book_entries",
+	label: __("Include Default FB Entries"),
+	fieldtype: "Check",
+	default: 1,
+});
