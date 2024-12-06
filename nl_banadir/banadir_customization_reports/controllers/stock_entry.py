8cf6141cc386b626758d9ae7d48cdb51017f43b9
maniamartial <martialamania19@gmail.com>
2024-12-06 12:08:38 +0300

    fix - Amount equal it to base amount on additional cost

--- nl_banadir/banadir_customization_reports/controllers/stock_entry.py
+++ nl_banadir/banadir_customization_reports/controllers/stock_entry.py
@@ -3,18 +3,20 @@ import frappe
 
 def add_total_operation_cost(doc):
     operation_cost = get_operation_cost(doc.work_order)
-    expense_account = frappe.get_doc("Company", doc.company).custom_default_operating_cost
-
+    expense_account = frappe.get_doc("Company", doc.company).default_operating_cost_account
+    if not expense_account:
+        frappe.throw("Go to company and enter the respective default Operating Cost Account")
+    
     if operation_cost:
         existing_entry = next(
             (entry for entry in doc.additional_costs if entry.description == "Operation Cost"), None
         )
-        
         if not existing_entry:
             doc.append("additional_costs", {
                 "expense_account": expense_account,
                 "description": "Operation Cost",
-                "amount": operation_cost
+                "amount": operation_cost,
+                "base_amount":operation_cost
             })
 
 
