42595e3ee5df21cb06de01f43d7ea23b2a5cf3c0
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-12-09 17:12:43 +0300

    (feat): sales_shipment_and_journal_difference report

---
+++ nl_banadir/banadir_customization_reports/report/sales_shipment_and_journal_difference/sales_shipment_and_journal_difference.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2024, Navari Ltd and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Shipment and Journal Difference"] = {
+	filters: [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1,
+		},
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+        },
+        {
+            "fieldname": "currency",
+            "label": __("Currency"),
+            "fieldtype": "Link",
+            "options": "Currency",
+        },
+        {
+            "fieldname": "journal_entry",
+            "label": __("Journal Entry"),
+            "fieldtype": "Link",
+            "options": "Journal Entry",
+        },
+        {
+            "fieldname": "sales_invoice",
+            "label": __("Sales Invoice"),
+            "fieldtype": "Link",
+            "options": "Sales Invoice",
+        },
+        {
+            "fieldname": "sales_shipment",
+            "label": __("Sales Shipment"),
+            "fieldtype": "Link",
+            "options": "Sales Shipment Cost",
+        }
+	],
+};
