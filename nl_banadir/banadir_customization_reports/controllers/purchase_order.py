e6495f7d2c08b77bf28a1c07b1afa293b7df2d01
maniamartial <martialamania19@gmail.com>
2024-12-10 13:52:02 +0300

    feat - fetch manufactured qty from work order related poroduction plan on purchase order

--- nl_banadir/banadir_customization_reports/controllers/purchase_order.py
+++ nl_banadir/banadir_customization_reports/controllers/purchase_order.py
@@ -22,3 +22,26 @@ def get_items_from_production_plan(production_plan):
     frappe.response['message'] = items
     return items
 
+'''I am against this method: how will we know 
+the specific work order to get the quantity from?'''
+
+@frappe.whitelist()
+def get_qty_from_first_work_order(production_plan):
+    """
+    Fetch items and quantities from the first Work Order associated with the given Production Plan.
+    """
+    work_order = frappe.get_all(
+        "Work Order",
+        filters={"production_plan": production_plan},
+        fields=["name", "production_item", "qty"],
+        order_by="creation",
+        limit_page_length=1
+    )
+
+    if work_order:
+        frappe.response["message"]=work_order[0].qty
+    else:
+        return None
+    
+    
+
