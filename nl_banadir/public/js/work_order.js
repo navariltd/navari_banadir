8fe895436592cd9dcd7b7bf7f9a5d68ee88ebb10
maniamartial <martialamania19@gmail.com>
2025-01-08 17:36:16 +0300

    feat - validate in progress date and complted date

--- nl_banadir/public/js/work_order.js
+++ nl_banadir/public/js/work_order.js
@@ -1,3 +1,28 @@
+frappe.ui.form.on('Work Order', {
+    refresh: function (frm) {
+        if (frm.doc.custom_subcontractors) {
+            frm.doc.custom_subcontractors.forEach(row => {
+                const fields_to_update = ['status', 'item', 'rate', 'supplier', 'in_progress', 'completed_date'];
+                if (row.invoice_created == 1) {
+                    // Make specific fields readonly and disallow changes on submit
+                    fields_to_update.forEach(field => {
+                        frappe.meta.get_docfield('Work Order Operations Item', field, frm.doc.name).read_only = 1;
+                        frappe.meta.get_docfield('Work Order Operations Item', field, frm.doc.name).allow_on_submit = 0;
+                    });
+                } else {
+                    // Make fields editable and allow changes on submit
+                    fields_to_update.forEach(field => {
+                        frappe.meta.get_docfield('Work Order Operations Item', field, frm.doc.name).read_only = 0;
+                        frappe.meta.get_docfield('Work Order Operations Item', field, frm.doc.name).allow_on_submit = 1;
+                    });
+                }
+            });
+
+            // Refresh the child table to apply changes
+            frm.refresh_field('custom_subcontractors');
+        }
+    }
+});
 
 
 frappe.ui.form.on('Work Order Operations Item', {
@@ -14,5 +39,7 @@ frappe.ui.form.on('Work Order Operations Item', {
         } else {
             frappe.msgprint(__('Please select a company in the Work Order.'));
         }
-    }
+    },
+   
 });
+
