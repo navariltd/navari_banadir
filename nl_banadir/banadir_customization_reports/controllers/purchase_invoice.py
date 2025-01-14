416730a8d6cf91f16c23e10979585e07e649f2fc
maniamartial <martialamania19@gmail.com>
2025-01-14 17:09:20 +0300

    fix - Only autoname the Banadir Steel Ltd purchase invoice

--- nl_banadir/banadir_customization_reports/controllers/purchase_invoice.py
+++ nl_banadir/banadir_customization_reports/controllers/purchase_invoice.py
@@ -3,12 +3,13 @@ from frappe.model.naming import make_autoname
 import frappe
 
 def auto_name(doc, method=None):
-    company_abbr = frappe.db.get_value("Company", doc.company, "abbr")
-    if not company_abbr:
-        frappe.throw(f"Company abbreviation not found for {doc.company}")
+    if doc.company=="Banadir Steel LTD":
+        company_abbr = frappe.db.get_value("Company", doc.company, "abbr")
+        if not company_abbr:
+            frappe.throw(f"Company abbreviation not found for {doc.company}")
 
-    current_year = datetime.now().year
+        current_year = datetime.now().year
 
-    base_name = make_autoname(f"{company_abbr}-.####")
-    doc.name = f"{base_name}-{current_year}"
-   
+        base_name = make_autoname(f"{company_abbr}-.####")
+        doc.name = f"{base_name}-{current_year}"
+    
