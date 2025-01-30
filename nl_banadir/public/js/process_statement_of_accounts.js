83c826baf52d3f55a5c71283d5cd2452ba4faa45
wafula <wafulaat@gmail.com>
2025-01-30 12:31:52 +0300

    feat: add functionality to send emails for 'Process Statement of Accounts' using a template without remarks.

--- nl_banadir/public/js/process_statement_of_accounts.js
+++ nl_banadir/public/js/process_statement_of_accounts.js
@@ -1,23 +1,55 @@
 frappe.ui.form.on("Process Statement Of Accounts", {
   refresh(frm) {
-    frm.add_custom_button(__("Custom Download"), function () {
-      if (frm.is_dirty()) frappe.throw(__("Please save before proceeding."));
-      let url = frappe.urllib.get_full_url(
-        "/api/method/nl_banadir.banadir_customization_reports.overrides.process_of_statements.download_statements?" +
-          "document_name=" +
-          encodeURIComponent(frm.doc.name)
-      );
-      $.ajax({
-        url: url,
-        type: "GET",
-        success: function (result) {
-          if (jQuery.isEmptyObject(result)) {
-            frappe.msgprint(__("No Records for these settings."));
-          } else {
-            window.location = url;
-          }
+    if (!frm.doc.__islocal) {
+      frm.add_custom_button(
+        "Send Emails",
+        function () {
+          if (frm.is_dirty())
+            frappe.throw(__("Please save before proceeding."));
+          frappe.call({
+            method:
+              "nl_banadir.banadir_customization_reports.overrides.process_of_statements.send_emails",
+            args: {
+              document_name: frm.doc.name,
+            },
+            callback: function (r) {
+              if (r && r.message) {
+                frappe.show_alert({
+                  message: __("Emails Queued"),
+                  indicator: "blue",
+                });
+              } else {
+                frappe.msgprint(__("No Records for these settings."));
+              }
+            },
+          });
         },
-      });
-    });
+        __("No Remarks")
+      );
+      frm.add_custom_button(
+        __("Download"),
+        function () {
+          if (frm.is_dirty())
+            frappe.throw(__("Please save before proceeding."));
+          let url = frappe.urllib.get_full_url(
+            "/api/method/nl_banadir.banadir_customization_reports.overrides.process_of_statements.download_statements?" +
+              "document_name=" +
+              encodeURIComponent(frm.doc.name)
+          );
+          $.ajax({
+            url: url,
+            type: "GET",
+            success: function (result) {
+              if (jQuery.isEmptyObject(result)) {
+                frappe.msgprint(__("No Records for these settings."));
+              } else {
+                window.location = url;
+              }
+            },
+          });
+        },
+        __("No Remarks")
+      );
+    }
   },
 });
