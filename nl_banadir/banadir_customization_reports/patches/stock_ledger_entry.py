d07eef79790543c0e7c476ed16b4e6a653b0b9b8
maniamartial <martialamania19@gmail.com>
2025-01-30 08:50:05 +0300

    feat - Change fiscal year on stock ledger entries

---
+++ nl_banadir/banadir_customization_reports/patches/stock_ledger_entry.py
@@ -0,0 +1,11 @@
+import frappe
+
+def execute():
+    frappe.db.sql("""
+        UPDATE `tabStock Ledger Entry`
+        SET fiscal_year = '2024'
+        WHERE posting_date BETWEEN '2025-01-01' AND '2025-01-23'
+        AND company = 'CITYWALK FOOTWEAR PVT LTD'
+    """)
+    frappe.db.commit()
+
