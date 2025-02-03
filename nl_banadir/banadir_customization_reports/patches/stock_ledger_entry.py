b31f79d518903bd4c783474a8351a6fc48158226
maniamartial <martialamania19@gmail.com>
2025-02-03 16:23:14 +0300

    feat - update on patches, new fiscal year

--- nl_banadir/banadir_customization_reports/patches/stock_ledger_entry.py
+++ nl_banadir/banadir_customization_reports/patches/stock_ledger_entry.py
@@ -3,8 +3,8 @@ import frappe
 def execute():
     frappe.db.sql("""
         UPDATE `tabStock Ledger Entry`
-        SET fiscal_year = '2024'
-        WHERE posting_date BETWEEN '2025-01-01' AND '2025-01-23'
+        SET fiscal_year = 'India Mar24 To 23/01/2025'
+        WHERE posting_date BETWEEN '2024-01-03' AND '2025-01-23'
         AND company = 'CITYWALK FOOTWEAR PVT LTD'
     """)
     frappe.db.commit()
