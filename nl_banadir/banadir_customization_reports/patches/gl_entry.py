a66bb3b30ba728977889e7850d559c6770595f29
maniamartial <martialamania19@gmail.com>
2025-02-05 14:51:11 +0300

    feat - create new patches for gl entry and stock ledger entry

--- nl_banadir/banadir_customization_reports/patches/gl_entry.py
+++ nl_banadir/banadir_customization_reports/patches/gl_entry.py
@@ -3,7 +3,7 @@ import frappe
 def execute():
     frappe.db.sql("""
         UPDATE `tabGL Entry`
-        SET fiscal_year = 'India Mar24 To 23/01/202'
+        SET fiscal_year = 'conakry 2024'
         WHERE posting_date BETWEEN '2024-07-01' AND '2025-01-30'
         AND company = 'Banadir General Trading LTD (Conakry)'
     """)
--- nl_banadir/banadir_customization_reports/patches/stock_ledger_entry.py
+++ nl_banadir/banadir_customization_reports/patches/stock_ledger_entry.py
@@ -3,7 +3,7 @@ import frappe
 def execute():
     frappe.db.sql("""
         UPDATE `tabStock Ledger Entry`
-        SET fiscal_year = 'India Mar24 To 23/01/2025'
+        SET fiscal_year = 'conakry 2024'
         WHERE posting_date BETWEEN '2024-07-01' AND '2025-01-30'
         AND company = 'Banadir General Trading LTD (Conakry)'
     """)
